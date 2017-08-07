28de18a7031c87b06e65daf9a40f24a7faa4e1a0
yadoch <yadoch@gmail.com>
2017-08-07 08:52:16 +0800

    Touch 面板產生球的功能完成

--- app/src/main/java/tw/com/abc/myjavatest/MainActivity.java
+++ app/src/main/java/tw/com/abc/myjavatest/MainActivity.java
@@ -7,31 +7,32 @@ import android.util.Log;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MyView myView;
+    private Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-   // Java 語法
-        LinkedList list = new LinkedList();
-        list.add(123);
-        list.add(123.45);
-        list.add("Brind");
-        list.add("Brind");
-        list.add(true);
-        Log.i("geoff",list.toString());
 
-        HashSet set=new HashSet();
-        set.add(123);
-        set.add(123.45);
-        set.add("Brind");
-        set.add("Brind");
-        set.add(true);
-        Log.i("geoff",set.toString());
-         */
+        timer=new Timer();
+        myView =(MyView) findViewById(R.id.myView);
+        myView.setTimer(timer);
+
     }
 
+    @Override
+    public void finish() {
+        // 結束時中止timer
+        //封裝關係移到MainActivity
+       // Timer timer = myView.getTimer();
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+
+        super.finish();
+    }
 }
--- app/src/main/java/tw/com/abc/myjavatest/MyView.java
+++ app/src/main/java/tw/com/abc/myjavatest/MyView.java
@@ -8,47 +8,126 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
-import java.sql.Time;
+// import java.sql.Time;
+import java.util.LinkedList;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyView extends View {
     private Resources res;
-    private Bitmap ballBMP;
-    private int ViewW,ViewH;
-    private float ballW,ballH,dx,dy;
-    private Boolean isInit;
+    private Bitmap[] ballBMP;
+    private int[] ballRes={R.drawable.ball0,R.drawable.ball1,R.drawable.ball2,R.drawable.ball3};
+    private int viewW,viewH;
+    private float ballW,ballH;
+    public boolean isInit;
     private Matrix matrix;
     private Timer timer;
+    private LinkedList<BallTask> balls;
+
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        res= context.getResources();
-        ballBMP=BitmapFactory.decodeResource(res,R.drawable.ball);
+        setBackgroundResource(R.drawable.mybg);
+
+        res = context.getResources();
+
+        //原本為Bitmap 變成Bitmap[] 的處理
+        ballBMP=new Bitmap[ballRes.length];
+        for (int i = 0; i < ballRes.length; i++){
+            ballBMP[i] = BitmapFactory.decodeResource(res, ballRes[i]);
+        }
         matrix=new Matrix();
-        timer=new Timer();
+
+        balls=new LinkedList<>();
+        //封裝關係移到MainActivity
+        //timer=new Timer();
+        //timer.schedule(new BallTask(),500,30);
     }
+
+    //
+    public void setTimer(Timer timer){
+        this.timer = timer;
+      //  timer.schedule(new BallTask(),1000,60);
+    }
+
+    //傳出timer 給Activity 控制結束--> getTimer() 會自動帶出
+    //封裝關係移到MainActivity
+    //public Timer getTimer() {return timer;}
+
     public void init(){
-        ViewH=getHeight();ViewW=getWidth();
-        ballW=ViewW /8f; ballH=ballW;
+        isInit=true;
+        viewH=getHeight();viewW=getWidth();
+        // 控制球的大小
+        ballW=viewW /8f; ballH=ballW;
+        // 變形前先reset();必免之前的值帶入
+
         matrix.reset();
-        matrix.postScale(ballW / ballBMP.getWidth(),ballH/ballBMP.getHeight());
+        matrix.postScale(ballW / ballBMP[0].getWidth(),ballH/ballBMP[0].getHeight());
+        for (int i=0;i<ballBMP.length;i++) {
+            ballBMP[i] = Bitmap.createBitmap(
+                    ballBMP[i], 0, 0, ballBMP[i].getWidth(), ballBMP[i].getHeight(),
+                    matrix, false);
+        }
+       // dx=dy=8;
 
-        ballBMP=Bitmap.createBitmap(ballBMP,0,0,ballBMP.getWidth(),ballBMP.getHeight(),matrix,false);
+    }
+    private class BallTask extends TimerTask {
+        private float ballX,ballY,dx,dy;
+        private int intBall;
+        // 建構式 33:45 ,Class 中的 方法,讓外部程式呼叫
+        // 用途:放置每個球的資訊框架,搭配LinkedSet 和迴圈用來存取每個球的資訊
+        BallTask(int intBall,float ballX,float ballY,float dx,float dy){
+            this.intBall=intBall;
+            this.ballX= ballX;
+            this.ballY= ballY;
+            this.dx=dx;
+            this.dy=dy;
+        }
 
-            
-        dx=dy=0;
 
-}
+        @Override
+        public void run() {
+            if(ballX<0 || ballX + ballW > viewW){
+                dx *= -1;
+            }
+            if(ballY<0 || ballY + ballH > viewH){
+                dy *= -1;
+            }
+            ballX+=dx;
+            ballY+=dy;
+            postInvalidate();
+        }
+    }
+//BallTask Class END ===
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float ex=event.getX();float ey=event.getY();
+        BallTask ball = new BallTask((int)(Math.random()*4),ex,ey,16,16);
+        balls.add(ball);
+        timer.schedule(ball,0,30);
+        return false;
+        //return super.onTouchEvent(event);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if(!isInit) init();
 
         //Log.i("geoff","Vx:"+Vx+"Vy"+Vy);
-        canvas.drawBitmap(ballBMP,0,0,null);
+
+        // 透過foreach 把 balls 的資訊放入 ball 中,在此ball 代表每一個球的資訊
+        for(BallTask ball : balls) {
+            // 用 ball 的屬性帶入值
+            canvas.drawBitmap(ballBMP[ball.intBall], ball.ballX, ball.ballY, null);
+        }
+        // 原本一個球的程式,多個球註解掉
+        //canvas.drawBitmap(ballBMP, ballX, ballY, null);
+
     }
 }
